95dc6b24cdd659dbdd3b7431488c6c243e07a1d7
Demis Bellot <demis.bellot@gmail.com>
2017-06-16 23:46:08 -0400

    remove unused

--- nuget/v4.5.10/deploy/nuget-deploy.bat
+++
@@ -1,7 +0,0 @@
-SET NUGET=C:\src\ServiceStack\src\.nuget\nuget
-%NUGET% setApiKey 34ca0303-aa1e-4b39-85ff-eeaebbdec600
-
-%NUGET% push ServiceStack.OrmLite.Firebird.4.5.10.nupkg -Source https://www.nuget.org/api/v2/package
-%NUGET% push ServiceStack.OrmLite.Oracle.4.5.10.nupkg -Source https://www.nuget.org/api/v2/package
-%NUGET% push ServiceStack.OrmLite.Oracle.Signed.4.5.10.nupkg -Source https://www.nuget.org/api/v2/package
-%NUGET% push ServiceStack.OrmLite.VistaDb.4.5.10.nupkg -Source https://www.nuget.org/api/v2/package
